5669a74360e992f2f7887435a6afcb26a0598df8
Harsh Joshi <108806522+harshjoshi01@users.noreply.github.com>
2023-10-20 17:09:25 +0530

    Update FindCurses.cmake

--- cmake/Modules/FindCurses.cmake
+++ cmake/Modules/FindCurses.cmake
@@ -1,5 +1,5 @@
 # Custom FindCurses.cmake module
-find_library(CURSES_LIBRARY NAMES ncurses PATHS /usr/lib/aarch64-linux-gnu)
+find_library(CURSES_LIBRARY NAMES ncurses PATHS /usr/lib/arm-linux-gnueabihf)
 find_path(CURSES_INCLUDE_DIR NAMES curses.h PATHS /usr/include)
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Curses REQUIRED_VARS CURSES_LIBRARY CURSES_INCLUDE_DIR)
